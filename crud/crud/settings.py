182895464f82cbfbf871c2533b0847dc82582503
Talha1818 <muhammadtaha171717@gmail.com>
2022-08-08 12:57:57 +0500

    Deployement Testing

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9dligtp@(ckn=xh-l@kkcx)f8%0y2*vnxn&d)5(#1c@&kk87v@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
